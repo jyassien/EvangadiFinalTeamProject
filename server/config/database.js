e4f9d101fee90391ba779f31253f1c31bc7460db
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-07-30 23:44:21 -0600

    Database configeration edited with hidden input values.

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,11 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  host: "127.0.0.1", // process.env.DB_HOST,
-  user: "users", // process.env.DB_USER,
-  password: "users1234", //  process.env.DB_PASS,
-  database: "users", //  process.env.MYSQL_DB,
+
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
   connectionLimit: 10,
 });
 
